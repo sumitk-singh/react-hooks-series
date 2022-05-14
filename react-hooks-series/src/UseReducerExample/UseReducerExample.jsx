e6cc39a6014d6a16912f12b23c583018899c77c7
sumit-0-0-1 <sumit.email.dev@gmail.com>
2022-05-14 19:55:05 +0530

    updated

---
+++ react-hooks-series/src/UseReducerExample/UseReducerExample.jsx
@@ -0,0 +1,5 @@
+// What is the useReducer hook?
+
+// useReducer is an alternative to useState, It is an efficient placeholder to store our state. Also, 
+// it provides you more control to manage the state.
+// useReducer and reducer are different
